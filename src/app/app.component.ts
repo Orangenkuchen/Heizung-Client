79369a12f313836cb936f9c12dca8912da3767d7
root <Orangenkuchen>
2024-10-31 17:45:57 +0000

    Fehlende Dateien eingecheckt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { LoggerService } from './services/Logger/logger.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'Heizungsdaten';
+
+    public constructor(loggerService: LoggerService)
+    {
+
+    }
+
+    title = 'Heizungsdaten';
 }
